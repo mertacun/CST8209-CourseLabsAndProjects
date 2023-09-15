a94842cde933b4232076bb88010110d54c058793
Mert Halil Acun <acun0008@algonquinlive.com>
2023-09-15 17:13:25 -0400

    Completes Lab 2

--- lab-2/script.js
+++ lab-2/script.js
@@ -1,5 +1,6 @@
 let firstName = 'Mert';
 console.log('My name is ' + firstName);
+
 let favNumber = 7;
 let squared = favNumber**2;
-console.log('The squared of ' + favNumber + ' is ' + squared);
+console.log(`The squared of ${favNumber} is ${squared}`);
